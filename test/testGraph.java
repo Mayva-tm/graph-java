f67851eb42c9b9e07db4fa232ccf105a11ccfd50
mayva tm <mticonam@est.emi.edu.bo>
2020-10-14 15:52:30 -0400

    12/10/20 mayerli ticona

--- test/testGraph.java
+++ test/testGraph.java
@@ -11,7 +11,35 @@ public class testGraph {
         graph.BFS(graph.getVertexList().getHead().getData());
     }
 
+    static void testPrintPrimero(){
+
+        Graph graph = new Graph(false);
+        graph.readFileInput("test.txt");
+        graph.printGraph();
+        graph.BFS(graph.getVertexList().getHead().getData());
+    }
+
+    static void testPrintSegundo(){
+
+        Graph graph = new Graph(false);
+        graph.readFileInput("test2.txt");
+        graph.printGraph();
+        graph.BFS(graph.getVertexList().getHead().getData());
+    }
+
+
+
+
     public static void main(String[] args) {
         testPrintAndrea();
+        //primera prueba
+        testPrintPrimero();
+        testPrintSegundo();
+
+
+
+
+
+
     }
 }
